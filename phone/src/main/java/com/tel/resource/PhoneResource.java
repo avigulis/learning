3b53a9c06942770cfb82b4638b1e1078bb16636e
avigulis <myxamor@gmail.com>
2017-06-09 10:23:22 +0300

    Set http response type to json. Spring will do serialisation internally.

--- phone/src/main/java/com/tel/resource/PhoneResource.java
+++ phone/src/main/java/com/tel/resource/PhoneResource.java
@@ -6,6 +6,7 @@ import com.tel.model.PhoneValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.Errors;
@@ -24,7 +25,7 @@ public class PhoneResource {
 
     private static final Logger log = LoggerFactory.getLogger(PhoneResource.class);
 
-    @GetMapping("/{phone}")
+    @GetMapping(value = "/{phone}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<ObjectNode> phone(@NotNull @Size(min = 1)
                                                 @PathVariable(value="phone") String phone) {
         log.debug("phone => {}", phone);
