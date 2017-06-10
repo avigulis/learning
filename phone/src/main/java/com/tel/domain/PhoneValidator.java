a4fffffdec241cddd71602db6fdd1f954beadb10
avigulis <myxamor@gmail.com>
2017-06-10 09:05:50 +0300

    Phone validator refactoring. Removed SpringContext don't need, can autowire application context without calling static method.

--- phone/src/main/java/com/tel/domain/PhoneValidator.java
+++ phone/src/main/java/com/tel/domain/PhoneValidator.java
@@ -5,84 +5,75 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
 import com.neovisionaries.i18n.CountryCode;
 import com.tel.service.PhoneService;
-import com.tel.util.SpringContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-import org.springframework.validation.Errors;
 
 import java.math.BigInteger;
-import java.util.Optional;
+import java.util.Objects;
+
 
 @Component
 public class PhoneValidator {
     private static final Logger log = LoggerFactory.getLogger(PhoneValidator.class);
     private static final PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
 
-    private String number;
 
-    private String country;
+    private final PhoneService phoneService;
 
-    public String validate(Object target) {
+    @Autowired
+    public PhoneValidator(PhoneService phoneService) {
+        this.phoneService = phoneService;
+    }
 
-        Errors errors = null;
-        Assert.notNull(target, "Target must not be null");
+    public String validate(String phone) {
 
-        PhoneValidator phoneValidator = (PhoneValidator) target;
-        log.info("Begin custom validation: " + phoneValidator.toString());
-        PhoneService phoneService = SpringContext.get().getBean(PhoneService.class);
+        Objects.nonNull(phone);
+
+        log.info("Begin custom validation: {}", phone);
+
 
         // Validate for number
         BigInteger number = null;
         try {
-            number = new BigInteger(phoneValidator.getNumber().replaceAll("[^0-9]", ""));
+            number = new BigInteger(phone.replaceAll("[^0-9]", ""));
         } catch (NumberFormatException e) {
-            log.info("Number is not a string: " + phoneValidator.getNumber());
-            errors.rejectValue("number", "", "number.invalid");
+            log.error("Number is not a string: {}", phone);
+            //errors.rejectValue("number", "", "number.invalid");
+            throw new RuntimeException("number.invalid");
         }
 
         // Validate for country
-        Optional<String> country = phoneService.get(number);
-        if (country.isPresent()) phoneValidator.setCountry(country.get());
-        else {
-            errors.rejectValue("number", "", "country.invalid");
-        }
-
-        log.info("Number has country: " + country.get());
+        String country = phoneService.get(number).orElseThrow(() -> {
+            //errors.rejectValue("number", "", "country.invalid");
+            return new RuntimeException("country.invalid");
+        });
+        log.debug("Number has country: {}", country);
 
         // Validate for phone
-        boolean countryValid = CountryCode.findByName(country.get()).size() > 0;
-        log.info("Country code available? " + countryValid);
+        boolean countryValid = CountryCode.findByName(country).size() > 0;
+        log.debug("Country code available? {}", countryValid);
         Phonenumber.PhoneNumber phoneNumber = null;
         try {
             if (countryValid) {
-                CountryCode countryCode = CountryCode.findByName(country.get()).get(0);
-                phoneNumber = phoneUtil.parse(phoneValidator.getNumber(), countryCode.getAlpha2());
+                CountryCode countryCode = CountryCode.findByName(country).get(0);
+                phoneNumber = phoneUtil.parse(phone, countryCode.getAlpha2());
             } else {
-                phoneNumber = phoneUtil.parse(phoneValidator.getNumber(), country.get());
+                phoneNumber = phoneUtil.parse(phone, country);
             }
         } catch (NumberParseException e) {
-            errors.rejectValue("number", "", "number.invalid");
+            //errors.rejectValue("number", "", "number.invalid");
+            throw new RuntimeException("number.invalid");
         }
 
         if (!phoneUtil.isValidNumber(phoneNumber)) {
-            errors.rejectValue("number", "", "number.invalid");
+            //errors.rejectValue("number", "", "number.invalid");
+            throw new RuntimeException("number.invalid");
         }
 
         log.info("Number valid");
-        return null;
+        return country;
     }
 
-    public String getNumber() { return number; }
-    public void setNumber(String number) { this.number = number; }
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
-
-    @Override
-    public String toString() {
-        return "PhoneValidator{" +
-                "number='" + number + '\'' +
-                '}';
-    }
 }
